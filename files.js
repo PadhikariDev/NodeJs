da11df12170dbfb82bb535634b12c46089f30d35
PadhikariDev <prashant.kyler99@gmail.com>
2025-04-30 16:49:42 +0530

    architecture of node js and how it works

---
+++ files.js
@@ -0,0 +1,38 @@
+// //files handling is node js 
+
+// //can create files and read them
+
+// //need modules named fs to work on with file handling
+// import fs from 'fs';
+
+// // //creating file
+// // const f= fs.writeFileSync("fileTest.txt","creating files and overding it ");
+
+// //reading the existing file
+
+// // const r= fs.readFileSync("./fileTest.txt","utf-8");
+
+// // //deleting the files
+// // const d =fs.unlinkSync("fileTest.txt");
+
+// // can append to existing file
+// // const ap = fs.appendFileSync("fileTest.txt",`\n${new Date()}`)
+
+//some of the used ones are : Reading files: fs.readFile(), fs.readFileSync()
+
+// Writing to files: fs.writeFile(), fs.writeFileSync()
+
+// Appending to files: fs.appendFile(), fs.appendFileSync()
+
+// Deleting files: fs.unlink(), fs.unlinkSync()
+
+// Directory handling: fs.mkdir(), fs.rmdir(), fs.readdir()
+
+// File renaming: fs.rename()
+
+// Getting file stats: fs.stat()
+
+// File access check: fs.access()
+
+// export default {ap};
+
---
+++ server.js
@@ -0,0 +1,9 @@
+// //importing modules
+
+// import extra from "./extra.js";
+// // //importing file handling 
+// // import ap from "./files.js";
+
+// console.log(extra.sub(2,5));
+
+
