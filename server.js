97431e0147ce69440138c4edabab38b71f5a983f
PadhikariDev <prashant.kyler99@gmail.com>
2025-05-01 14:54:40 +0530

    http server

--- server.js
+++ server.js
@@ -7,3 +7,17 @@
 // console.log(extra.sub(2,5));
 
 
+//creating http server
+import http from "http";
+import fs from "fs"
+const httpServer =http.createServer((req , res)=>{
+    const result = `${Date.now()}:Request Received : ${req.method}\n`
+    fs.appendFile("log.txt",result,(err)=>{
+    console.log("new log has been created");   
+    res.end("hello from the server side");
+    })
+});
+
+httpServer.listen(3000, ()=>{
+    console.log("server has been started")
+})
